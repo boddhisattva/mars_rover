420c1c41f5416b448db8507bfad9219cbdd3d73d
Mohnish G J <mail4mohnish@gmail.com>
2024-06-16 16:40:21 +0200

    Add Rover position class to capture details of a rovers current position

---
+++ lib/rover_position.rb
@@ -0,0 +1,7 @@
+class RoverPosition
+  def initialize(x_coordinate, y_coordinate, orientation)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @orientation = orientation
+  end
+end
