ece5035f5ae9b5ccd8a1dc44dc11541e8c915639
Mohnish G J <mail4mohnish@gmail.com>
2024-06-16 09:17:00 +0200

    Add ability to require lib files when running relevant tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,12 @@
 
 require 'pry'
 
+PROJECT_ROOT = File.expand_path('../..', __FILE__)
+
+Dir.glob(File.join(PROJECT_ROOT, 'lib', '*.rb')).each do |file|
+  require file
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
