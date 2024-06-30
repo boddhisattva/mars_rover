b25ad1e70ad62a7538de0750853e1998b0961bbc
Mohnish G J <mail4mohnish@gmail.com>
2024-06-30 20:05:02 +0200

    Refactor code further & update README
    - Navigating the steps in the path is the Responsibility of the Path class
    - Vehicle clan send a message to the specified vehicle which in this case is Rover to accordingly turn left, or right
    - Update README to see how the app is fairing with regard to its flog score with regard to different methods and compare that with a reference point as per the blog here: https://jakescruggs.blogspot.com/2008/08/whats-good-flog-score.html

--- spec/lib/path_spec.rb
+++ spec/lib/path_spec.rb
@@ -12,4 +12,27 @@ describe Path do
   it 'has a MOVE constant' do
     expect(Path::MOVE).to eq('M')
   end
+
+  describe '#navigate_steps' do
+    let(:terrain) { RectangularPlateau.new(0, 0, 5, 5) }
+
+    context 'when a vehicle pointing in a specific direction has a path to navigate' do
+      it 'navigates each step within that path' do
+        path1 = described_class.new('LMLMLMLMM')
+        rover1 = Rover.new(Position.new(1, 2, 'N', terrain))
+        path1.navigate_steps(rover1, North.new)
+
+        expect(rover1.current_location).to eq('1 3 N')
+      end
+    end
+
+    context 'when a vehicle travels through an invalid path in a given terrain' do
+      it 'returns an Invalid Step Error error with an appropriate error message related to an invalid step' do
+        path1 = described_class.new('LMLMLSMLMM')
+        rover1 = Rover.new(Position.new(1, 2, 'N', terrain))
+
+        expect { path1.navigate_steps(rover1, North.new) }.to raise_error(InvalidStepError, 'Invalid step in path')
+      end
+    end
+  end
 end
